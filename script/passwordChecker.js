11ee32bc58127b0226bc49097ab217878651b887
truuue <noahvernhet@gmail.com>
2024-01-25 11:06:50 +0100

    Major update : Add new styles and views for login and registration pages

---
+++ script/passwordChecker.js
@@ -0,0 +1,9 @@
+function passwordChecker() {
+    var password = prompt("Veuillez entrer le mot de passe pour accéder à cette page :");
+    if(password == "motdepasse") {
+    } else {
+        alert("Mot de passe incorrect.");
+        window.location = "index.html"; // Redirection si le mot de passe est incorrect
+    }
+}
+passwordChecker();
