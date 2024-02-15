05007209b464617029e99e13abd37e7a4292c05a
truuue <noahvernhet@gmail.com>
2024-02-15 13:51:41 +0100

    Add burger menu functionality

---
+++ static/js/burgerMenu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let navWrapper = document.querySelector(".nav-wrapper"),
+    navToogler = document.querySelector(".nav-toogler");
+
+  navToogler.addEventListener("click", function () {
+    navWrapper.classList.toggle("active");
+  });
+});
