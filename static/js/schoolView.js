5d970c3cc248f2fc8adba02cb76242302eb33a25
truuue <noahvernhet@gmail.com>
2024-03-14 00:08:31 +0100

    Add styles and functionality for school view page

---
+++ static/js/schoolView.js
@@ -0,0 +1,11 @@
+function showImage(src) {
+    const modal = document.getElementById("modal");
+    const fullImage = document.getElementById("fullImage");
+    fullImage.src = src.replace("/thumb/", "/full/");
+    modal.style.display = "block";
+  }
+  
+  function closeImage() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
